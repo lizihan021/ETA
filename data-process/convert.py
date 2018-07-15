ae94b09ed09a07e1ce615d3a06ee8c30e6d36f6c
hewen <hewen@umich.edu>
2018-07-15 20:23:32 +0800

    convert util

--- data-process/convert.py
+++ data-process/convert.py
@@ -1,11 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# this file is used to convert GPS data to map_matching data
-# useage: 
+"""
+convert.py
+
+Facilitate convertion from raw GPS data to mapbox readable style file.
+
+WARNING: convert.py does not automatically convert to style file (JSON file),
+         it only convert part of the JSON file.
+"""
+
 import sys
 
-if __name__ == "__main__":
-    with open("9f2f5a3972df52464e93495dbd528c80_result.json", 'r') as f:
+"""
+This function converts data into geojson file.
+
+Sample data input:
+104.084655 30.656510 0 21003
+104.084391 30.656630 1 21003
+
+Sample data output:
+{
+    "type":"Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [104.084655, 30.656510]
+    },
+    "properties": {
+        "titles": "0\n21003"
+    }
+},
+{
+    "type":"Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [104.084391, 30.656630]
+    },
+    "properties": {
+        "titles": "1\n21003"
+    }
+},
+"""
+geo_convert(in_file):
+    with open(in_file, 'r') as f:
         f_w = open("goodgeopoints", 'w')
         for line in f:
             line = line.strip()
@@ -22,12 +58,28 @@ if __name__ == "__main__":
             f_w.write(w_str)
         f_w.close()
 
+"""
+This function converts data into geojson file.
 
-    with open("9f2f5a3972df52464e93495dbd528c80.json", 'r') as f:
+Sample data input:
+104.084655 30.656510
+104.084391 30.656630
+
+Sample data output:
+[104.08466,30.65652],
+[104.08440,30.65665],
+"""
+ptspath_convert(in_file):
+    with open(in_file, 'r') as f:
         f_w = open("goodpoints", 'w')
         for line in f:
             line = line.strip()
             newline = line.replace(" ", ",")
             f_w.write("[" + newline + "],\n")
         f_w.close()
+
+
+if __name__ == "__main__":
+    geo_convert("9f2f5a3972df52464e93495dbd528c80_result.json")
+    ptspath_convert("9f2f5a3972df52464e93495dbd528c80.json")
 
