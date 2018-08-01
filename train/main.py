9d1053343b5a82a2c9ed91dfe4b91b5ee14d7e6d
lizihan <lizihan@umich.edu>
2018-08-01 12:18:25 +0800

    yo

--- train/main.py
+++ train/main.py
@@ -6,10 +6,11 @@ from road import RoadDataset
 import os
 import pickle
 import sys
-from utils import get
+from utils import get, mov_avg
 from road import RoadDataset
 from cnn import *
 import shutil
+import matplotlib.pyplot as plt
 
 # Initialize global variables
 WEIGHTS_FOLDER = get('cnn.weights_folder')
@@ -17,7 +18,7 @@ VALIDATION_SPLIT = get('cnn.validation_split')
 BATCH_SIZE = get('cnn.batch_size')
 CNN_EPOCHS = get('cnn.cnn_epochs')
 DATA_FOLDER = get('data_folder')
-np.set_printoptions(edgeitems=6)
+np.set_printoptions(edgeitems=30)
 
 class bcolors:
     HEADER = '\033[95m'
@@ -29,7 +30,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def train():
+def train(train_flag = True):
     # read filenames
     try:
         os.mkdir(WEIGHTS_FOLDER)
@@ -38,12 +39,12 @@ def train():
     filenames = [x for _, _, x in os.walk(DATA_FOLDER)]
     for filename in filenames[0]:
         # define input data:
-        if filename.split(".")[-1] == 'p':
+        if filename.split(".")[-1] == 'p' and filename == "185075051_5459472401_2039561245.p":
             print "\n" + filename
-            train_road(filename)
+            train_road(filename, train_flag)
 
 
-def train_road(filename):
+def train_road(filename, train_flag = True):
     road = RoadDataset(filename)
     x_train, y_train, x_test, y_test, row_id_train, row_id_test, timestamp_train, timestamp_test = road.load_data()
     # print x_train[1]
@@ -55,17 +56,18 @@ def train_road(filename):
     model = generate_model()
     plot_model(model, to_file='model.png')
 
-    callbacks = [ModelCheckpoint(WEIGHTS_FOLDER + "weights_" + filename, 
-                                 monitor='val_loss', save_best_only=True)]
-    history = model.fit([x_train, timestamp_train], y_train,
-              batch_size=BATCH_SIZE,
-              epochs=CNN_EPOCHS,
-              verbose=0,
-              validation_split=VALIDATION_SPLIT,
-              callbacks=callbacks)
+    if train_flag:
+        callbacks = [ModelCheckpoint(WEIGHTS_FOLDER + "weights_" + filename, 
+                                     monitor='val_loss', save_best_only=True)]
+        history = model.fit([x_train, timestamp_train], y_train,
+                  batch_size=BATCH_SIZE,
+                  epochs=CNN_EPOCHS,
+                  verbose=0,
+                  validation_split=VALIDATION_SPLIT,
+                  callbacks=callbacks)
 
-    max_val_loss, idx = min((val, idx) for (idx, val) in enumerate(history.history['val_loss']))
-    print('Min validation loss = {0:.4f} (epoch {1:d})'.format(max_val_loss, idx+1))
+        max_val_loss, idx = min((val, idx) for (idx, val) in enumerate(history.history['val_loss']))
+        print('Min validation loss = {0:.4f} (epoch {1:d})'.format(max_val_loss, idx+1))
 
     model.load_weights(WEIGHTS_FOLDER + "weights_" + filename)
 
@@ -73,6 +75,24 @@ def train_road(filename):
     print 'Test loss:', score[0], 'Test mae:', score[1]
 
     predict_result = model.predict([x_test, timestamp_test], batch_size=BATCH_SIZE)
+    # print predict_result
+    # print y_test
+    plot_pred = [z[1] for z in predict_result[250:480]]
+    plot_y = [z[1] for z in y_test[250:480]]
+    mavg = mov_avg(plot_pred, 9)
+    err = 0;
+    for i, tmp1 in enumerate(plot_y):
+        err = err + abs(tmp1-mavg[i])
+    print "mae: ", err / len(plot_y)
+    x_points = xrange(len(plot_y))
+    # fig = plt.figure()
+    # ax = fig.add_subplot(211)
+    plt.plot(x_points, plot_y, 'b', label="actual")
+    plt.plot(x_points, plot_pred, 'r', label="CNN predict")
+    plt.plot(x_points, mavg, 'y', label="moving avg")
+    plt.legend()
+    plt.show()
+
     print bcolors.WARNING + "Test error %: ", score[1] / np.mean(y_test), bcolors.ENDC
     if  score[1] / np.mean(y_test) > 0.5:
         print "BAO ZHA LA !!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -87,4 +107,7 @@ if __name__ == "__main__":
     argv = sys.argv + [None] * 2
     if argv[1] == 'clean':
         clean()
-    train()
+    if argv[1] == 'notrain':
+        train(False)
+    else:
+        train()
